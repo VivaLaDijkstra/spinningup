624d0bf7836876fb1e14558aec9c23932c3c6c4d
wuhanghao <wuhanghao@gmail.com>
2024-06-02 18:37:47 +0800

    style: black and isort

--- spinup/algos/pytorch/ddpg/core.py
+++ spinup/algos/pytorch/ddpg/core.py
@@ -1,6 +1,4 @@
 import numpy as np
-import scipy.signal
-
 import torch
 import torch.nn as nn
 
@@ -10,15 +8,18 @@ def combined_shape(length, shape=None):
         return (length,)
     return (length, shape) if np.isscalar(shape) else (length, *shape)
 
+
 def mlp(sizes, activation, output_activation=nn.Identity):
     layers = []
-    for j in range(len(sizes)-1):
-        act = activation if j < len(sizes)-2 else output_activation
-        layers += [nn.Linear(sizes[j], sizes[j+1]), act()]
+    for j in range(len(sizes) - 1):
+        act = activation if j < len(sizes) - 2 else output_activation
+        layers += [nn.Linear(sizes[j], sizes[j + 1]), act()]
     return nn.Sequential(*layers)
+
 
 def count_vars(module):
     return sum([np.prod(p.shape) for p in module.parameters()])
+
 
 class MLPActor(nn.Module):
 
@@ -32,6 +33,7 @@ class MLPActor(nn.Module):
         # Return output from network scaled to action space limits.
         return self.act_limit * self.pi(obs)
 
+
 class MLPQFunction(nn.Module):
 
     def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
@@ -40,12 +42,18 @@ class MLPQFunction(nn.Module):
 
     def forward(self, obs, act):
         q = self.q(torch.cat([obs, act], dim=-1))
-        return torch.squeeze(q, -1) # Critical to ensure q has right shape.
+        return torch.squeeze(q, -1)  # Critical to ensure q has right shape.
+
 
 class MLPActorCritic(nn.Module):
 
-    def __init__(self, observation_space, action_space, hidden_sizes=(256,256),
-                 activation=nn.ReLU):
+    def __init__(
+        self,
+        observation_space,
+        action_space,
+        hidden_sizes=(256, 256),
+        activation=nn.ReLU,
+    ):
         super().__init__()
 
         obs_dim = observation_space.shape[0]
